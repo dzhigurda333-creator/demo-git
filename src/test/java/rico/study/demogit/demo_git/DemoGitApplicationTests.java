7fa5681b66293edd526379353055ab896fbb0d60
Rico_the_developer <dzhigurda333@gmail.com>
2026-05-05 23:01:32 +0500

    init commit

---
+++ src/test/java/rico/study/demogit/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package rico.study.demogit.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
